b1406df4682337ccc890130659e097f294f658f5
Peter Bandsholm <lil.jdean@gmail.com>
2021-04-01 23:14:27 +0200

    collapsing logic optimized

--- script.js
+++ script.js
@@ -14,16 +14,14 @@ function productionTab(event, idOfElement){
     
     console.log(event, idOfElement)
     $('#produktioner-underRubrik').slideToggle(500, function(e){
-        // const element = document.querySelector('.tabhover')
-        // const style = getComputedStyle(element)
-        // var color = style.listStyleType;
-        // console.log(color)
-        // if(color === 'disc'){
-        //     $( element ).css( "list-style-type", "square");
-        // }else $( element ).css( "list-style-type", "disc");
 
-        // const type = style.list-style-type
-        // console.log(type + "test")
+        console.log(getComputedStyle(document.querySelector('#kontakt-underRubrik')).display)
+        if(getComputedStyle(document.querySelector('#produktioner-underRubrik')).display === 'block'  
+        && getComputedStyle(document.querySelector('#kontakt-underRubrik')).display === 'block')
+        {
+            $( "#kontakt-underRubrik:not(#produktioner-underRubrik)").slideToggle( "slow")
+
+        }
     });
 
     // var x = document.getElementById("produktioner-underRubrik");
@@ -43,7 +41,8 @@ function kontaktTab(event, idOfElement) {
     $('#kontakt-underRubrik').slideToggle(500, function(e){
 
         console.log(getComputedStyle(document.querySelector('#produktioner-underRubrik')).display)
-        if((getComputedStyle(document.querySelector('#produktioner-underRubrik')).display === 'block'))
+        if(getComputedStyle(document.querySelector('#kontakt-underRubrik')).display === 'block'  
+        && getComputedStyle(document.querySelector('#produktioner-underRubrik')).display === 'block')
         {
             $( "#produktioner-underRubrik:not(#kontakt-underRubrik)").slideToggle( "slow")
 
